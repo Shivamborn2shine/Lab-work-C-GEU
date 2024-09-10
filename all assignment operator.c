e28760d67e9a185e3bb07ddc256f1a58a27c0050
CHeatiiT <161832522+Shivamborn2shine@users.noreply.github.com>
2024-09-10 12:42:13 +0530

    Add files via upload

---
+++ all assignment operator.c
@@ -0,0 +1,13 @@
+//write a program to demonstrate all the arithematic operator
+#include <stdio.h>
+
+int main()
+{
+    int a = 10, b = 3;
+    printf("a + b = %d\n", a + b);
+    printf("a - b = %d\n", a - b);
+    printf("a * b = %d\n", a * b);
+    printf("a / b = %d\n", a / b);
+    printf("a %% b = %d\n", a % b);
+    return 0;
+}
